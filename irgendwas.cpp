5706782eb6e8c18fd99a8a5d19fe92d099a1a9cd
8-Fabian <fabian.denke@gmail.com>
2024-02-03 17:36:01 +0100

    abc

--- irgendwas.cpp
+++ irgendwas.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include "irgendwas.hpp"
+
+
+Testklasse::Testklasse(int ca, int cb){
+    a = ca;
+    b = cb;
+}
+
+int Testklasse::berechnen(){
+    return a + b;
+}
 
 int main(){
     
     std::cout << "Hallo Welt" << std::endl;
+
+    Testklasse testobjekt = Testklasse(10, 15);
+    std::cout << testobjekt.berechnen() << std::endl;
 
     return 0;
 }
---
+++ irgendwas.hpp
@@ -0,0 +1,13 @@
+class Testklasse{
+
+    public:  
+    Testklasse(int ca, int cb);
+    int berechnen();
+
+    private:   
+    int a;
+    int b;
+
+
+
+};
